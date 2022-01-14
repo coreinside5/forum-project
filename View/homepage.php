01dfc064fe2831ce52f6ea71fa7de8ee65aa4cbd
Maxim K <90226452+coreinside5@users.noreply.github.com>
2022-01-14 16:32:20 +0100

    eod version

--- View/homepage.php
+++ View/homepage.php
@@ -1,20 +1,20 @@
 <?php require 'includes/header.php' ?>
-
-<section>
-    <h1>Forum</h1>
-    <?php
-    foreach ($posts as $post) : ?>
-        <article class="posts">
-            <!--<form action="./index.php?action=post&id=<?php echo $post["id"] ?>" method="post">
+<div class="mainbody">
+    <section>
+        <h1>Forum</h1>
+        <?php
+        foreach ($posts as $post) : ?>
+            <article class="posts">
+                <!--<form action="./index.php?action=post&id=<?php echo $post["id"] ?>" method="post">
                 <input type="hidden" name="user_id" value="<?php echo $post["id"] ?>" />
                 <button><?php echo $post["id"]; ?></button>
-            </form>-->    
+            </form>-->
 
-            <div class="post__name"><a class="postitem" href="./index.php?action=post&id=<?php echo $post["id"] ?>"><?php echo $post["name"]; ?></a></div>
-            <div class="post__text"><?php echo $post["text"]; ?></div>
-            <div class="post__signature"><?php echo $post["signature"]; ?></div>
-        </article>
-    <?php endforeach; ?>
-</section>
-
+                <div class="post__name"><a class="postitem" href="./index.php?action=post&id=<?php echo $post["id"] ?>"><?php echo $post["name"]; ?></a></div>
+                <div class="post__text"><?php echo $post["text"]; ?></div>
+                <div class="post__signature"><?php echo $post["signature"]; ?></div>
+            </article>
+        <?php endforeach; ?>
+    </section>
+</div>
 <?php require 'includes/footer.php' ?>
--- View/includes/header.php
+++ View/includes/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>M@x-forum</title>
+    <title>Tea-forum</title>
     <link rel="stylesheet" type="text/css" href="./src/styles/style.css" />
     <link rel="shortcut icon" href="./src/images/favicon.ico" type="image/x-icon" />
 </head>
@@ -13,6 +13,6 @@
 <body>
     <header>
         <div class="header">
-            <h1>tracker</h1>
+            <h1>TEA</h1>
         </div>
     </header>
